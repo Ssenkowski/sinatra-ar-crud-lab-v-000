9f4cee7d30d84f38917265b7a1c1e2813c8ac87e
Steven Senkowski <scsenkowski@gmail.com>
2018-01-09 17:52:54 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -4,9 +4,13 @@ class Post < ActiveRecord::Base
   def self.all
     @@all ||= []
   end
+  
   def initialize(opts={})
     @name, @content = opts[:name], opts[:content]
   end
 
+  def save
+
+  end
 
 end
