bdec5064c85b713012e759be9b8a0cb73e2f82ac
Steven Senkowski <scsenkowski@gmail.com>
2018-01-09 17:52:34 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
   attr_accessor :name, :content
 
-  def initialize()
-    @name, @content = name, content
+  def self.all
+    @@all = []
+  end
+  def initialize(opts={})
+    @name, @content = opts[:name], opts[:content]
   end
 
 
