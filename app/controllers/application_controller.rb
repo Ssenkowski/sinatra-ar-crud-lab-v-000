5fbfbb5240cf6681fff1a4e8541a564aaea935d2
Steven Senkowski <scsenkowski@gmail.com>
2018-01-09 18:02:15 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
 
   get '/posts' do
     @posts = Post.all
-      @posts.map { |post| post(params[:name]) post(params[:content])}
+      @posts.map { |post| post{params[:name]} post{params[:content]}}
 
     erb :index
   end
